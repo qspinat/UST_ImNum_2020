0fee0605aeeab251605597a4d519d349692a0fe7
qspinat <qspinat@gmail.com>
2021-01-27 20:10:54 +0100

    alpha variable

--- PytorchWCT/new_func.py
+++ PytorchWCT/new_func.py
@@ -11,6 +11,7 @@ import torch
 import argparse
 from PIL import Image
 import torch
+import numpy as np
 from torch.autograd import Variable
 import torchvision.utils as vutils
 import torchvision.datasets as datasets
@@ -42,40 +43,43 @@ def styleTransfer(contentImg,styleImg,csF,alpha=0.5, device = torch.device("cuda
     args = SimpleNamespace(vgg1=vgg1, vgg2=vgg2, vgg3=vgg3, vgg4=vgg4, vgg5=vgg5, decoder1=decoder1, decoder2=decoder2, decoder3=decoder3, decoder4=decoder4, decoder5=decoder5)
 
     wct = WCT(args).to(device)
+    
+    if len(np.shape(alpha))==0:
+        alpha = [alpha,alpha,alpha,alpha,alpha]
 
     sF5 = wct.e5(styleImg)
     cF5 = wct.e5(contentImg)
     sF5 = sF5.data.cpu().squeeze(0)
     cF5 = cF5.data.cpu().squeeze(0)
-    csF5 = wct.transform(cF5,sF5,csF,alpha)
+    csF5 = wct.transform(cF5,sF5,csF,alpha[0])
     Im5 = wct.d5(csF5)
 
     sF4 = wct.e4(styleImg)
     cF4 = wct.e4(Im5)
     sF4 = sF4.data.cpu().squeeze(0)
     cF4 = cF4.data.cpu().squeeze(0)
-    csF4 = wct.transform(cF4,sF4,csF,alpha)
+    csF4 = wct.transform(cF4,sF4,csF,alpha[1])
     Im4 = wct.d4(csF4)
 
     sF3 = wct.e3(styleImg)
     cF3 = wct.e3(Im4)
     sF3 = sF3.data.cpu().squeeze(0)
     cF3 = cF3.data.cpu().squeeze(0)
-    csF3 = wct.transform(cF3,sF3,csF,alpha)
+    csF3 = wct.transform(cF3,sF3,csF,alpha[2])
     Im3 = wct.d3(csF3)
 
     sF2 = wct.e2(styleImg)
     cF2 = wct.e2(Im3)
     sF2 = sF2.data.cpu().squeeze(0)
     cF2 = cF2.data.cpu().squeeze(0)
-    csF2 = wct.transform(cF2,sF2,csF,alpha)
+    csF2 = wct.transform(cF2,sF2,csF,alpha[3])
     Im2 = wct.d2(csF2)
 
     sF1 = wct.e1(styleImg)
     cF1 = wct.e1(Im2)
     sF1 = sF1.data.cpu().squeeze(0)
     cF1 = cF1.data.cpu().squeeze(0)
-    csF1 = wct.transform(cF1,sF1,csF,alpha)
+    csF1 = wct.transform(cF1,sF1,csF,alpha[4])
     Im1 = wct.d1(csF1)
     # save_image has this wired design to pad images with 4 pixels at default.
     #vutils.save_image(Im1.data.cpu().float(),os.path.join(args.outf,imname))
